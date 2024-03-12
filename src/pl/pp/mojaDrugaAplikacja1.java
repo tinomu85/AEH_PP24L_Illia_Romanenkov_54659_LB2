5a00a779939252595393c3b227be501192ccafa2
tinomu85 <122554767+tinomu85@users.noreply.github.com>
2024-03-12 16:20:02 +0100

    Add files via upload

---
+++ src/pl/pp/mojaDrugaAplikacja1.java
@@ -0,0 +1,96 @@
+/*
+package pl.pp;
+
+import java.util.Scanner;
+public class mojaDrugaAplikacja1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Proszę podaj swoje imię:");
+        String forename = scanner.nextLine();
+
+        System.out.println("Proszę podaj swoje nazwisko:");
+        String surname = scanner.nextLine();
+
+        scanner.close();
+        System.out.println("Witaj " + forename + " " + surname);
+    }
+    }
+*/
+
+
+/*
+package pl.pp;
+
+import java.util.Scanner;
+public class mojaDrugaAplikacja1 {
+    public static void main(String[] args) {
+        double x = 15;
+        double y = 2;
+
+        Scanner scanner = new Scanner(System.in);
+
+        var result= x + y;
+        System.out.println("x + y = " + result);
+
+        result = x -y;
+        System.out.println("x -y = " + result);
+        result = x * y;
+        System.out.println("x * y = " + result);
+        result = x / y;
+        System.out.println("x / y = " + result);
+        result = x % y;
+        System.out.println("x % y = " + result);
+
+
+        System.out.println("Wpisz dwie liczby rozdzielone klawiszem Enter:");
+        double first = scanner.nextDouble();
+        double second = scanner.nextDouble();
+         System.out.println("x + y = " + (first + second));
+    }
+    }*/
+
+/*
+package pl.pp;
+public class mojaDrugaAplikacja1{
+public static void main(String[] args) {
+
+    int x = 11;
+
+
+    double doubleValue = 2 * x;
+
+
+    int xSquared = x * x;
+
+    System.out.println("dwukrotność liczby x : " + doubleValue);
+    System.out.println("wartość x^2: " + xSquared);
+}
+}
+*/
+
+
+
+/*
+package pl.pp;
+import java.util.Scanner;
+
+public class mojaDrugaAplikacja1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Wpisz swój wiek w latach: ");
+        int ageInYears = scanner.nextInt();
+
+        scanner.close();
+
+        long ageInSeconds = (long) ageInYears * 365 * 24 * 60 * 60;
+
+        System.out.println("Twój wiek w sekundach: " + ageInSeconds);
+    }
+}
+*/
+
+
+
